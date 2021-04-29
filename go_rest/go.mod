73f1d5759a53c4eca3c17eed556bb278240ab0a1
Purushotham <purushohtam.prathapa@gmail.com>
2021-04-29 23:00:52 +0530

    Initial logging implemented with Zap.

--- go_rest/go.mod
+++ go_rest/go.mod
@@ -5,5 +5,8 @@ go 1.16
 require (
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/lib/pq v1.10.1 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/spf13/viper v1.7.1 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.16.0 // indirect
 )
--- go_rest/main.go
+++ go_rest/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/spf13/viper"
+	"go.uber.org/zap"
 )
 
 func main() {
+	config := zap.NewProductionConfig()
+	config.OutputPaths = []string{"porus.log"}
+	logger, err := config.Build()
+
+	if err != nil {
+		log.Fatalf("can't initialize zap logger: %v", err)
+	}
+	defer logger.Sync()
 
 	viper.SetConfigName("config")
 	viper.AddConfigPath(".")
@@ -17,8 +27,9 @@ func main() {
 
 	a := App{}
 
+	logger.Info(fmt.Sprintf("Reading from config : database %s, user %s, password %s", viper.GetString("database"), viper.GetString("user"), viper.GetString("password")))
 	a.Initialize(viper.GetString("database"), viper.GetString("user"), viper.GetString("password"))
-
+	logger.Info("Initialization completed...")
 	a.Run(viper.GetString("host"))
 
 }
